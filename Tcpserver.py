f4748e8de3f9855c7474d3bc12d69c5ea2a7b113
Yottaqubyter <soyla@terra.com>
2017-03-09 21:06:16 +0100

    Tcp server
    Este programa es un programa hecho en python para montar un server TCP.
    Cuando recibe una conexión entrante y un paquete de datos tcp, te pide
    una respuesta para enviarle al cliente. Admite máximo de 1 conexión a la
    vez

---
+++ Tcpserver.py
@@ -0,0 +1,38 @@
+import socket, sys
+errlvl=False
+TcpServer=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while(True):
+    print("Escribe la ip en la que escuchar(Por defecto en localhost):\n --Si solo quieres que se conecte tu PC, localhost.\n --Si quieres que se puedan conectar los dispositivos de la red local, escribe tu IP local("+socket.gethostbyname(socket.gethostname())+").\n --Si quieres que se pueda conectar todo internet, escribe tu IP publica(http://miip.es/)")
+    ipl=input(">>> ")
+    if(ipl==""):
+        ipl="localhost"
+    print("Escribe el puerto por el que se va a escuchar(Por defecto es el 5555)")
+    portl=input(">>> ")
+    if(portl==""):
+        portl=5555
+    else:
+        try:
+            portl=int(portl)
+        except Exception as e:
+            errlvl=True
+    if(not errlvl):
+        try:
+            TcpServer.bind((ipl, portl))
+            break
+        except Exception as e:
+            print("Error: La IP o el Puerto no son validos")
+    else:
+        print("Error: La IP o el puerto no son validos")
+TcpServer.listen(1)
+while(True):
+    connection, client_address=TcpServer.accept()
+    try:
+        print("Conexion TCP entrante de", client_address[0]+"\nRecibiendo datos:")
+        while(True):
+            TcpMsg=connection.recv(256)
+            print(TcpMsg)
+            ans=input("Escribe una Respuesta: ")
+            connection.sendall(bytes(ans, 'utf-8'))
+    except Exception as e:
+        connection.close()
+    
